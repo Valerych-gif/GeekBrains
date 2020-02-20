8b4c39611ab184b57d12aa60764c3cbfd30c5664
Valery Bulekov <muzaccord@gmail.com>
2020-02-20 07:35:23 +0300

    Реализация Ai через рекурсию

--- HomeWorks/src/ru/valerych/tictactoe/Ai.java
+++ HomeWorks/src/ru/valerych/tictactoe/Ai.java
@@ -7,42 +7,120 @@ public class Ai {
     private Map map;
     private Game game;
     private Random random = new Random();
+    Cell lastHumanTurn;
+
 
     Ai(Map map, Game game) {
         this.map = map;
         this.game = game;
     }
 
-    public void aiTurn() {
+    public void aiTurn(int tries) {
+        Cell cell;
+        lastHumanTurn = game.getLastHumanTurn();
 
-        tryToWin();
-        tryToDoNotLose();
-        doRandomTurn();
+        if ((cell=tryToWin(1))!=null){
+            cell.putZero();
+            return;
+        } else if ((cell=tryToDoNotLose(1))!=null){
+            System.out.println("tryToDoNotLose");
+            cell.putZero();
+            return;
+        }
 
-    }
-
-    private void tryToWin (){
-
-        for (int i = 0; i < map.size; i++) {
-            for (int j = 0; j < map.size; j++) {
-
-                if (map.isCellValid(i+1, j+1) && isWinnable(i+1, j+1)) {
+        for (int i = 0; i < map.size-2; i++) {
+            for (int j = 0; j < map.size - 2; j++) {
+                if (map.isCellValid(i+1, j+1)&&horizontalDanger(i, j)) {
                     map.cells[i][j].putZero();
                     return;
                 }
-            }
-        }
-    }
-
-    private void tryToDoNotLose (){
-        for (int i = 0; i < map.size; i++) {
-            for (int j = 0; j < map.size; j++) {
-                if (map.isCellValid(i+1, j+1) && isLosable(i+1, j+1)) {
+                if (map.isCellValid(i+1,j+1)&&verticalDanger(i, j)) {
                     map.cells[i][j].putZero();
                     return;
                 }
+                if (map.isCellValid(i+1,j+1)&&mainDiagonalDanger(i, j)) {
+                    map.cells[i][j].putZero();
+                    return;
+                }
+//                if (reverseDiagonalDanger(i, j)) {
+//                    map.cells[i][j].putZero();
+//                    return;
+//                }
             }
         }
+
+        for (int i = tries; i > 1; i--) {
+            if ((cell=tryToWin(i))!=null){
+                cell.putZero();
+                return;
+            } else if ((cell=tryToDoNotLose(i))!=null){
+                cell.putZero();
+                return;
+            }
+
+        }
+            doRandomTurn();
+    }
+
+    private boolean verticalDanger(int x, int y) {
+        for (int i = 0; i < game.dotsForWin-2; i++) {
+            if (map.cells[x+i+1][y].getType()!=CellType.CROSS){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean horizontalDanger(int x, int y) {
+        for (int i = 0; i < game.dotsForWin-2; i++) {
+            if (map.cells[x][y+i+1].getType()!=CellType.CROSS){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean mainDiagonalDanger(int x, int y) {
+        for (int i = 0; i < game.dotsForWin-2; i++) {
+            if (map.cells[x+i+1][y+i+1].getType()!=CellType.CROSS){
+                return false;
+            }
+        }
+        return true;
+    }
+
+//    private boolean reverseDiagonalDanger(int x, int y) {
+//        for (int i = 0; i < game.dotsForWin-2; i++) {
+//            if (map.cells[x+i+1][y+i+1].getType()!=CellType.CROSS){
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+
+    private Cell tryToWin (int tries){
+
+        if (tries<=0) return null;
+        for (int i = 0; i < map.size; i++) {
+            for (int j=0; j < map.size; j++) {
+                if (map.isCellValid(i+1, j+1) && isWinnable(i+1, j+1, tries)) {
+                     return map.cells[i][j];
+                }
+            }
+        }
+        return null;
+    }
+
+    private Cell tryToDoNotLose (int tries){
+        if (tries<=0) return null;
+        for (int i = 0; i < map.size; i++) {
+            for (int j=0; j < map.size; j++) {
+                if (map.isCellValid(i+1, j+1) && isLosable(i+1, j+1, tries)) {
+                    return map.cells[i][j];
+                }
+            }
+        }
+        return null;
     }
 
     private void doRandomTurn(){
@@ -55,23 +133,36 @@ public class Ai {
         map.cells[rowNumber - 1][colNumber - 1].putZero();
     }
 
-    private boolean isLosable(int rowNumber, int colNumber) {
+    private boolean isLosable(int rowNumber, int colNumber, int tries) {
 
         map.cells[rowNumber - 1][colNumber - 1].putCross();
+
         if (game.checkWin(CellType.CROSS)) {
             map.cells[rowNumber - 1][colNumber - 1].clear();
             return true;
+        } else {
+            if (tryToDoNotLose(--tries)!=null) {
+                map.cells[rowNumber - 1][colNumber - 1].clear();
+                return true;
+            }
+            map.cells[rowNumber - 1][colNumber - 1].clear();
+            return false;
         }
-        map.cells[rowNumber - 1][colNumber - 1].clear();
-        return false;
+
     }
 
-    private boolean isWinnable(int rowNumber, int colNumber) {
+    private boolean isWinnable(int rowNumber, int colNumber, int tries) {
+
         map.cells[rowNumber - 1][colNumber - 1].putZero();
+
         if (game.checkWin(CellType.ZERO)) {
             map.cells[rowNumber - 1][colNumber - 1].clear();
             return true;
         } else {
+            if (tryToWin(--tries)!=null) {
+                map.cells[rowNumber - 1][colNumber - 1].clear();
+                return true;
+            }
             map.cells[rowNumber - 1][colNumber - 1].clear();
             return false;
         }
